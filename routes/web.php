49ce1730f5a059d8c8772e0b9c671bd1e5f97bb1
inaciosacataria <54165291+inaciosacataria@users.noreply.github.com>
2022-09-21 14:39:13 +0200

    Login view
    route /login

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/login', function (){
-     return view('welcome');
-});
 Route::get('/', function () {
-    return view('home');
+    return view('welcome');
+});
+
+
+Route::get('/login', function (){
+     return view('home');
 });
