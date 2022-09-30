46f7a4d6f6ee2d6c4fccbe1b1f6585b40218ef63
luzepio <luzepio@gmail.com>
2022-09-30 09:25:36 +0200

    js formatacao de texto.

--- resources/views/layouts/appmain.blade.php
+++ resources/views/layouts/appmain.blade.php
@@ -210,7 +210,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-12">
-                        © Copyright 2020 - Portal do Concurso. Todos os Direitos Reservados
+                        © Copyright 2022 - Motoristasco.mz. Todos os Direitos Reservados
                     </div>
                 </div>
             </div>
@@ -252,6 +252,8 @@
        <!-- Datatable init js -->
        <script src="{{asset('assets/pages/datatables.init.js')}}"></script>
 
+       <!--tinymce js-->
+       <script src="{{asset('plugins/tinymce/tinymce.min.js')}}"></script>
         <!-- App js -->
         <script src="{{asset('assets/js/app.js')}}"></script>
 
@@ -299,6 +301,34 @@
             $(".sp-loading").hide();
         });
 
+        </script>
+        <script>
+            $(document).ready(function () {
+
+
+                if($("#descricao").length > 0){
+                    tinymce.init({
+                        selector: "textarea#descricao",
+                        theme: "modern",
+                        height:300,
+                        plugins: [
+                            "advlist autolink link image lists charmap print preview hr anchor pagebreak spellchecker",
+                            "searchreplace wordcount visualblocks visualchars code fullscreen insertdatetime media nonbreaking",
+                            "save table contextmenu directionality emoticons template paste textcolor"
+                        ],
+                        toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | l      ink image | print preview media fullpage | forecolor backcolor emoticons",
+                        style_formats: [
+                            {title: 'Bold text', inline: 'b'},
+                            {title: 'Red text', inline: 'span', styles: {color: '#ff0000'}},
+                            {title: 'Red header', block: 'h1', styles: {color: '#ff0000'}},
+                            {title: 'Example 1', inline: 'span', classes: 'example1'},
+                            {title: 'Example 2', inline: 'span', classes: 'example2'},
+                            {title: 'Table styles'},
+                            {title: 'Table row 1', selector: 'tr', classes: 'tablerow1'}
+                        ]
+                    });
+                }
+            });
         </script>
 
         @yield('scripts')
