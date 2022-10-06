4f536e11e6d2cd0996d3933d5c76e71203000393
inaciosacataria <54165291+inaciosacataria@users.noreply.github.com>
2022-10-06 15:36:04 +0200

    Create, Read, Update(metodo para atualizar) ,Delete
    CRUD

--- app/Http/Controllers/CentralDeRiscoController.php
+++ app/Http/Controllers/CentralDeRiscoController.php
@@ -15,8 +15,13 @@ class CentralDeRiscoController extends Controller
 
     //    if(Auth::user()->privilegio=="empregador"){
 
+                $user_id = Auth::user()->id;
+                $empregador = DB::table('empregadores')
+                            ->where('empregadores.*', 'empregadores.user_id',$user_id)
+                            ->first();
+
                 $centralRisco = new CentralDeRisco;
-                $centralRisco->empregador_id=$request->empregador_id;
+                $centralRisco->empregador_id=empregador_id;
                 $centralRisco->candidato_id=$request->candidato_id;
                 $centralRisco->funcoes_do_candidato=$request->funcoes_do_candidato;
                 $centralRisco->infracao=$request->infracao;
@@ -61,5 +66,37 @@ class CentralDeRiscoController extends Controller
         }
 
     }
+
+
+    public function updateDenunciaMotorista(Request $request){
+
+
+    //    if(Auth::user()->privilegio=="empregador"){
+
+                $user_id = Auth::user()->id;
+                $empregador = DB::table('empregadores')
+                            ->where('empregadores.*', 'empregadores.user_id',$user_id)
+                            ->first();
+
+                $centralRisco = CentralDeRisco::find($request->id);
+                $centralRisco->empregador_id=empregador_id;
+                $centralRisco->candidato_id=$request->candidato_id;
+                $centralRisco->funcoes_do_candidato=$request->funcoes_do_candidato;
+                $centralRisco->infracao=$request->infracao;
+                $centralRisco->merece_portunidade=$request->merece_portunidade;
+                $centralRisco->versao_motorista=$request->versao_motorista;
+
+              if($centralRisco->update()){
+
+               return redirect()->back()->with('success', 'Motorista denunciado, os Admininstradores cuidaram do resto!');
+             }else{
+              return redirect()->back()->with('erro', 'Ocorreu erro, tenta novamente!');
+              }
+
+  //      }else {
+    //          return redirect()->back()->with('erro', 'Voce nao tem acesso a central de risco!');
+    //    }
+    }
+
 
 }
